374d534b1a1c0d4d65c2855a6292d474644a10fe
kroutony <kroutony@gmail.com>
2018-05-07 00:11:08 +0800

    Install laravel-tracy

--- config/app.php
+++ config/app.php
@@ -160,6 +160,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Third Party Providers
+         */
+        Recca0120\LaravelTracy\LaravelTracyServiceProvider::class,
+
     ],
 
     /*
